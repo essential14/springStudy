664f85a3db7bb56fbe6cb1b738a6ef87e9fd81cb
essential14 <essential193@gmail.com>
2023-12-06 15:55:29 +0900

    231206
    spring mvc 실습

--- springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
+++ springmvc/src/main/java/hello/springmvc/basic/LogTestController.java
@@ -1,17 +1,15 @@
 package hello.springmvc.basic;
 
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Slf4j
+//private final Logger log = LoggerFactory.getLogger(getClass()); 이걸 @Slf4j 어노테이션이 써줌
+@Slf4j
 @RestController
 public class LogTestController {
-
-    private final Logger log = LoggerFactory.getLogger(getClass());
+//    private final Logger log = LoggerFactory.getLogger(getClass());
 
     @RequestMapping("/log-test")
     public String logTest() {
@@ -19,15 +17,11 @@ public class LogTestController {
 
         System.out.println("name = " + name);
 
-        log.trace(" trace my log="+ name);
-
         log.trace("trace log={}", name);
-
         log.debug("debug log={}", name);
-        log.info(" info log={}", name);
-        log.warn(" warn log={}", name);
-        log.error("error log={}", name);
-
+        log.info("info log={}", name);
+        log.warn("warn log={}", name);
+        log.error("error log ={}", name);
         return "ok";
     }
 }
